a7471bf84189827c9471d22d9c84c03e8b6028fb
Exceret <15364051195@163.com>
2026-05-19 21:03:58 -0400

    update

--- deep_learning/mar_30_5/learning7.py
+++ deep_learning/mar_30_5/learning7.py
@@ -3,7 +3,7 @@ Title: learning7.py
 Content: 循环神经网络
 """
 
-from typing import Callable
+from typing import Callable, List, Tuple, Union
 from functools import reduce
 import numpy as np
 from learning6 import ReluActivator, IdentityActivator, element_wise_op
@@ -11,21 +11,27 @@ from utils.ts_print import ts_print
 
 
 class RecurrentLayer(object):
-    def __init__(self, input_width, state_width, activator, learning_rate):
-        self.input_width = input_width
-        self.state_width = state_width
+    def __init__(
+        self,
+        input_width: int,
+        state_width: int,
+        activator: Union[ReluActivator, IdentityActivator],
+        learning_rate: float,
+    ) -> None:
+        self.input_width: int = input_width
+        self.state_width: int = state_width
         self.activator = activator
-        self.learning_rate = learning_rate
-        self.times = 0  # 当前时刻初始化为t0
-        self.state_list = []  # 保存各个时刻的state
+        self.learning_rate: float = learning_rate
+        self.times: int = 0  # 当前时刻初始化为t0
+        self.state_list: List[np.ndarray] = []  # 保存各个时刻的state
         self.state_list.append(np.zeros((state_width, 1)))  # 初始化s0
-        self.U = np.random.uniform(-1e-4, 1e-4, (state_width, input_width))  # 初始化U
-        self.W = np.random.uniform(-1e-4, 1e-4, (state_width, state_width))  # 初始化W
-        self.gradient_list = []  # 保存各个时刻的权重梯度
-        self.delta_list = []
-        self.gradient = []
+        self.U: np.ndarray = np.random.uniform(-1e-4, 1e-4, (state_width, input_width))  # 初始化U
+        self.W: np.ndarray = np.random.uniform(-1e-4, 1e-4, (state_width, state_width))  # 初始化W
+        self.gradient_list: List[np.ndarray] = []  # 保存各个时刻的权重梯度
+        self.delta_list: List[np.ndarray] = []
+        self.gradient: np.ndarray = np.zeros((state_width, state_width))
 
-    def forward(self, input_array):
+    def forward(self, input_array: np.ndarray) -> None:
         """
         根据『式2』进行前向计算
         """
@@ -34,20 +40,20 @@ class RecurrentLayer(object):
         element_wise_op(state, self.activator.forward)
         self.state_list.append(state)
 
-    def backward(self, sensitivity_array, activator):
+    def backward(self, sensitivity_array: np.ndarray, activator: Union[ReluActivator, IdentityActivator]) -> None:
         """
         实现BPTT算法
         """
         self.calc_delta(sensitivity_array, activator)
         self.calc_gradient()
 
-    def update(self):
+    def update(self) -> None:
         """
         按照梯度下降，更新权重
         """
         self.W -= self.learning_rate * self.gradient
 
-    def calc_delta(self, sensitivity_array, activator):
+    def calc_delta(self, sensitivity_array: np.ndarray, activator: Union[ReluActivator, IdentityActivator]) -> None:
         """_summary_
 
         Args:
@@ -61,7 +67,7 @@ class RecurrentLayer(object):
         for k in range(self.times - 1, 0, -1):
             self.calc_delta_k(k, activator)
 
-    def calc_delta_k(self, k, activator):
+    def calc_delta_k(self, k: int, activator: Union[ReluActivator, IdentityActivator]) -> None:
         """
         根据k+1时刻的delta计算k时刻的delta
         """
@@ -71,7 +77,7 @@ class RecurrentLayer(object):
             np.dot(self.delta_list[k + 1].T, self.W), np.diag(state[:, 0])
         ).T
 
-    def calc_gradient(self):
+    def calc_gradient(self) -> None:
         """_summary_"""
         self.gradient_list = []  # 保存各个时刻的权重梯度
         for t in range(self.times + 1):
@@ -83,32 +89,32 @@ class RecurrentLayer(object):
             lambda a, b: a + b, self.gradient_list, self.gradient_list[0]
         )  # [0]被初始化为0且没有被修改过
 
-    def calc_gradient_t(self, t):
+    def calc_gradient_t(self, t: int) -> None:
         """
         计算每个时刻t权重的梯度
         """
         gradient = np.dot(self.delta_list[t], self.state_list[t - 1].T)
         self.gradient_list[t] = gradient
 
-    def reset_state(self):
+    def reset_state(self) -> None:
         """_summary_"""
         self.times = 0  # 当前时刻初始化为t0
         self.state_list = []  # 保存各个时刻的state
         self.state_list.append(np.zeros((self.state_width, 1)))  # 初始化s0
 
 
-def data_set():
+def data_set() -> Tuple[List[np.ndarray], np.ndarray]:
     """_summary_
 
     Returns:
-        _type_: _description_
+        Tuple[List[np.ndarray], np.ndarray]: _description_
     """
     x = [np.array([[1], [2], [3]]), np.array([[2], [3], [4]])]
     d = np.array([[1], [2]])
     return x, d
 
 
-def gradient_check():
+def gradient_check() -> None:
     """
     梯度检查
     """
@@ -149,7 +155,7 @@ def gradient_check():
             )
 
 
-def test():
+def test() -> RecurrentLayer:
     """_summary_
 
     Returns:
